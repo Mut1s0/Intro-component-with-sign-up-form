21fa2ef56e0cf3e3d96533a03262927605214464
Allan Musyoka <musyokaallan96@gmail.com>
2024-01-10 11:24:14 +0300

    Added templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "hsl( 0, 100%, 74% )",
+        green: "hsl( 154, 59%, 51% )",
+        blue: "hsl( 248, 32%, 49% )",
+        darkBlue: "hsl( 249, 10%, 26% )",
+        grayishBlue: "hsl( 246, 25%, 77% )"
+
+      },
+      fontFamily: {
+        poppins: [ 'Poppins', 'sans-serif']
+      },
+      fontWeight: {
+        regular: "400",
+        medium: "500",
+        semiBold: "600",
+        bold: "700"
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1024px",
+      lg: "1200px",
+      xl: "1700px",
+    }
   },
   plugins: [],
 }
