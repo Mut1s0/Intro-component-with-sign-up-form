c35f04c3ec5bf249ac0edfd7f2f67d1063959d9e
Allan Musyoka <musyokaallan96@gmail.com>
2024-01-10 21:01:18 +0300

    minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,9 +8,9 @@ function Header() {
 
             <div className="text-center md:text-left">
 
-                <h1 className="text-white font-semiBold text-[1.6rem] ss:text-[2.5rem] md:text-[3rem] mb-5">Learn to code by <br /> watching others</h1>
+                <h1 className="text-white font-semiBold text-[1.6rem] ss:text-[2.5rem] md:text-[2.7rem] mb-5">Learn to code by <br /> watching others</h1>
 
-                <p className="text-gray-200 font-medium text-[.6rem] ss:text-[1.2rem] md:text-[.9rem] lg:text-[1.4rem]">See how experienced developers solve <br className="block md:hidden"/> problems in real-time. <br className="hidden md:block"/> Watching <br className="block md:hidden"/> scripted tutorials is great, but <br className="block md:hidden"/> understanding how <br className="hidden md:block"/> developers think is <br className="block md:hidden"/> invaluable</p>
+                <p className="text-gray-200 font-medium text-[.6rem] ss:text-[1.2rem] md:text-[.9rem] lg:text-[1.2rem]">See how experienced developers solve <br className="block md:hidden"/> problems in real-time. <br className="hidden md:block"/> Watching <br className="block md:hidden"/> scripted tutorials is great, but <br className="block md:hidden"/> understanding how <br className="hidden md:block"/> developers think is <br className="block md:hidden"/> invaluable</p>
 
             </div>
 
